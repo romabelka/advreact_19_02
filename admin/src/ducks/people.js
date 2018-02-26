3b550fa1e8688da253cd1f3be54046c541a74743
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-02-26 19:32:34 +0200

    add simple saga

--- admin/src/ducks/people.js
+++ admin/src/ducks/people.js
@@ -2,13 +2,16 @@ import {appName} from '../config'
 import {Record, List} from 'immutable'
 import {reset} from 'redux-form'
 import {createSelector} from 'reselect'
+import {takeEvery, put, call} from 'redux-saga/effects'
+import {generateId} from './utils'
 
 /**
  * Constants
  * */
 export const moduleName = 'people'
 const prefix = `${appName}/${moduleName}`
-export const ADD_PERSON = `${prefix}/ADD_PERSON`
+export const ADD_PERSON_REQUEST = `${prefix}/ADD_PERSON_REQUEST`
+export const ADD_PERSON_SUCCESS = `${prefix}/ADD_PERSON_SUCCESS`
 
 /**
  * Reducer
@@ -28,8 +31,8 @@ export default function reducer(state = new ReducerState(), action) {
     const {type, payload} = action
 
     switch (type) {
-        case ADD_PERSON:
-            return state.update('entities', entities => entities.push(new PersonRecord(payload.person)))
+        case ADD_PERSON_SUCCESS:
+            return state.update('entities', entities => entities.push(new PersonRecord(payload)))
 
         default:
             return state
@@ -40,12 +43,20 @@ export default function reducer(state = new ReducerState(), action) {
  * */
 
 export const stateSelector = state => state[moduleName]
-export const peopleSelector = createSelector(stateSelector, state => state.entities.valueSeq().toArray())
+export const peopleSelector = createSelector(stateSelector, state => state.entities.toArray())
 
 /**
  * Action Creators
  * */
 
+export function addPerson(person) {
+    return {
+        type: ADD_PERSON_REQUEST,
+        payload: person
+    }
+}
+
+/*
 export function addPerson(person) {
     return (dispatch) => {
         dispatch({
@@ -57,4 +68,25 @@ export function addPerson(person) {
 
         dispatch(reset('person'))
     }
+}
+*/
+
+/**
+ * Sagas
+ **/
+
+export const addPersonSaga = function * (action) {
+    const id = yield call(generateId)
+
+    yield put({
+        type: ADD_PERSON_SUCCESS,
+        payload: {id, ...action.payload}
+    })
+
+    yield put(reset('person'))
+
+}
+
+export const saga = function * () {
+    yield takeEvery(ADD_PERSON_REQUEST, addPersonSaga)
 }
--- admin/src/redux/store.js
+++ admin/src/redux/store.js
@@ -2,12 +2,18 @@ import {createStore, applyMiddleware} from 'redux'
 import {routerMiddleware} from 'react-router-redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 import reducer from './reducer'
 import history from '../history'
+import {saga} from '../ducks/people'
 
-const enhancer = applyMiddleware(thunk, routerMiddleware(history), logger)
+const sagaMiddleware = createSagaMiddleware()
+
+const enhancer = applyMiddleware(thunk, sagaMiddleware, routerMiddleware(history), logger)
 
 const store = createStore(reducer, enhancer)
+
+sagaMiddleware.run(saga)
 
 window.store = store
 
