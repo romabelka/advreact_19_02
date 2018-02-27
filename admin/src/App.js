b4d0906858babc64f73d9e13fcf9125a083cfa9d
a-rusak <alexander.rusak@gmail.com>
2018-02-27 14:24:46 +0300

    HT2

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import AdminRoute from './components/routes/admin'
+import { userSelector } from './ducks/auth'
 import AuthRoute from './components/routes/auth'
-import {Route} from 'react-router-dom'
+import { Route, Redirect, Switch } from 'react-router-dom'
 import ProtectedRoute from './components/common/ProtectedRoute'
 import PersonPage from './components/routes/person-page'
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Route path='/auth' component={AuthRoute}/>
-                <ProtectedRoute path='/admin' component={AdminRoute}/>
-                <ProtectedRoute path="/people" component={PersonPage}/>
-            </div>
-        )
-    }
+  render() {
+    console.log(`---`, this.props.isAuthorized)
+    return (
+      <Switch>
+        <Route path="/auth" component={AuthRoute} />
+        <ProtectedRoute path="/admin" component={AdminRoute} />
+        <ProtectedRoute path="/people" component={PersonPage} />
+        {this.props.isAuthorized ? <Redirect from="/auth" to="/people" /> : null}
+      </Switch>
+    )
+  }
 }
 
-export default App
+// export default App
+
+export default connect(
+  state => ({
+    isAuthorized: userSelector(state)
+  }),
+  null,
+  null,
+  { pure: false }
+)(App)
