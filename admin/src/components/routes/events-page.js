e38a08a4321f466c773848fdd94b44b2f93d8acb
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-03-01 17:39:13 +0200

    add HW2

---
+++ admin/src/components/routes/events-page.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import EventsTable from '../events/events-table'
+
+class EventsPage extends Component {
+    static propTypes = {
+
+    };
+
+    render() {
+        return (
+            <div>
+                <EventsTable />
+            </div>
+        )
+    }
+}
+
+export default EventsPage
