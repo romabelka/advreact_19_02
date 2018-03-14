e03af4a3f8429ab52dfd4ba4241a68a3b52a4df0
alex_kart <alex_kart@mail.ru>
2018-03-15 00:31:22 +0200

    Task1

--- admin/src/ducks/auth.js
+++ admin/src/ducks/auth.js
@@ -1,9 +1,10 @@
-import {all, takeEvery, take, put, apply, call} from 'redux-saga/effects'
+import {all, takeEvery, take, put, apply, call, spawn} from 'redux-saga/effects'
 import {appName} from '../config'
 import {createSelector} from 'reselect'
 import {Record} from 'immutable'
 import firebase from 'firebase'
 import {replace} from 'react-router-redux'
+import {eventChannel} from 'redux-saga'
 
 /**
  * Constants
@@ -15,6 +16,11 @@ export const SIGN_IN_REQUEST = `${prefix}/SIGN_IN_REQUEST`
 export const SIGN_IN_START = `${prefix}/SIGN_IN_START`
 export const SIGN_IN_SUCCESS = `${prefix}/SIGN_IN_SUCCESS`
 export const SIGN_IN_ERROR = `${prefix}/SIGN_IN_ERROR`
+
+export const SIGN_OUT_REQUEST = `${prefix}/SIGN_OUT_REQUEST`
+export const SIGN_OUT_START = `${prefix}/SIGN_OUT_START`
+export const SIGN_OUT_SUCCESS = `${prefix}/SIGN_OUT_SUCCESS`
+export const SIGN_OUT_ERROR = `${prefix}/SIGN_OUT_ERROR`
 
 export const SIGN_UP_REQUEST = `${prefix}/SIGN_UP_REQUEST`
 export const SIGN_UP_START = `${prefix}/SIGN_UP_START`
@@ -36,6 +42,7 @@ export default function reducer(state = new ReducerRecord(), action) {
     switch (type) {
         case SIGN_IN_START:
         case SIGN_UP_START:
+        case SIGN_OUT_START:
             return state
                 .set('error', null)
                 .set('loading', true)
@@ -46,8 +53,15 @@ export default function reducer(state = new ReducerRecord(), action) {
                 .set('loading', false)
                 .set('user', payload.user)
 
+        case SIGN_OUT_SUCCESS:
+            return state
+                .set('loading', false)
+                .set('user', null)
+
+
         case SIGN_IN_ERROR:
         case SIGN_UP_ERROR:
+        case SIGN_OUT_ERROR:
             return state
                 .set('loading', false)
                 .set('error', payload.error.message)
@@ -84,12 +98,46 @@ export function signUp(email, password) {
     }
 }
 
-firebase.auth().onAuthStateChanged(user => {
+export function signOut() {
+    return {
+        type: SIGN_OUT_REQUEST
+    }
+}
+
+
+const createAuthChannel = () => eventChannel(emit =>
+    firebase.auth().onAuthStateChanged(user =>  emit({user}))
+)
+
+
+/*firebase.auth().onAuthStateChanged(user => {
     if (user) window.store.dispatch({
         type: SIGN_IN_SUCCESS,
         payload: { user }
     })
-})
+})*/
+
+
+const onAuthChangeSaga  = function* (){
+    const channel = yield call(createAuthChannel)
+    while (true){
+        const { user } = yield take(channel)
+        console.log(user);
+        if (user) {
+            yield put({
+                type: SIGN_IN_SUCCESS,
+                payload: {user}
+            })
+            yield (put(replace('/people')))
+        }
+        else {
+            yield put({
+                type: SIGN_OUT_SUCCESS
+            })
+            yield (put(replace('/auth/sign-in')))
+        }
+    }
+}
 
 /**
  * Sagas
@@ -116,6 +164,23 @@ export const signUpSaga = function * () {
     }
 }
 
+export const signOutSaga = function * () {
+
+    yield put({
+        type: SIGN_OUT_START
+    })
+
+    try {
+        const auth = firebase.auth()
+        yield apply(auth, auth.signOut)
+    } catch (error) {
+        yield put({
+            type: SIGN_OUT_ERROR,
+            payload: { error }
+        })
+    }
+}
+
 export const signInSaga = function * (action) {
     const { email, password } = action.payload
 
@@ -134,19 +199,22 @@ export const signInSaga = function * (action) {
     }
 }
 
-export function * watchStatusChangeSaga() {
+/*export function * watchStatusChangeSaga() {
     while (true) {
         yield take(SIGN_IN_SUCCESS)
 
         yield (put(replace('/people')))
     }
-}
+}*/
 
 
 export const saga = function * () {
+    yield spawn(onAuthChangeSaga)
+
     yield all([
         takeEvery(SIGN_IN_REQUEST, signInSaga),
+        takeEvery(SIGN_OUT_REQUEST, signOutSaga),
         signUpSaga(),
-        watchStatusChangeSaga()
+        //watchStatusChangeSaga(),
     ])
 }
