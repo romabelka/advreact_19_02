1f28cea013a2e81e067549259b9a277ad3212649
alex_kart <alex_kart@mail.ru>
2018-03-15 01:03:11 +0200

    Task2

--- admin/src/components/people/people-list.js
+++ admin/src/components/people/people-list.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {peopleListSelector, fetchAllPeople} from '../../ducks/people'
 import {List} from 'react-virtualized'
 import PersonCard from './person-card'
+import { TransitionMotion, spring } from 'react-motion'
 
 import 'react-virtualized/styles.css'
 
@@ -12,20 +13,38 @@ class PeopleList extends Component {
     }
 
     render() {
-        return <List
-            rowRenderer={this.rowRenderer}
-            rowCount={this.props.people.length}
-            rowHeight={100}
-            height={400}
-            width={400}
-        />
 
+
+        return  <TransitionMotion
+                styles = {this.getStyles()}
+                willLeave = {() => ({ opacity: spring(0, { stiffness: 10 })})}
+                willEnter = {() => ({ opacity: 0 })}
+            >
+                {interpolated => (
+                    <List
+                        rowRenderer={this.rowRenderer(interpolated)}
+                        rowCount={this.props.people.length}
+                        rowHeight={100}
+                        height={400}
+                        width={400}
+                    />
+                )}
+            </TransitionMotion>
     }
 
-    rowRenderer = ({ style, index, key }) => {
+
+    getStyles() {
+        return this.props.people.map(item => ({
+            key: item.uid,
+            data: item,
+            style: { opacity: spring(1, { stiffness: 30 })}
+        }))
+    }
+
+    rowRenderer = (interpolated) => ({ style, index, key }) => {
         const person = this.props.people[index]
         return (
-            <div style={style} key={key}>
+            <div style={{...style, ...interpolated}} key={key}>
                 <PersonCard person = {person}/>
             </div>
         )
