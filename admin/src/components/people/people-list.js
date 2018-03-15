49c283d03fab24ee71aa6a635231614aad39088c
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-03-15 17:38:59 +0200

    add HW5

--- admin/src/components/people/people-list.js
+++ admin/src/components/people/people-list.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {peopleListSelector, fetchAllPeople} from '../../ducks/people'
 import {List} from 'react-virtualized'
 import PersonCard from './person-card'
+import {TransitionMotion, spring} from 'react-motion'
 
 import 'react-virtualized/styles.css'
 
@@ -11,27 +12,53 @@ class PeopleList extends Component {
         this.props.fetchAllPeople()
     }
 
-    render() {
-        return <List
-            rowRenderer={this.rowRenderer}
-            rowCount={this.props.people.length}
-            rowHeight={100}
-            height={400}
-            width={400}
-        />
-
+    componentDidUpdate({people}) {
+        if (people.length && this.props.people.length > people.length) {
+            setTimeout(() => {
+                this.list.scrollToRow(this.props.people.length)
+            }, 0)
+        }
     }
 
-    rowRenderer = ({ style, index, key }) => {
-        const person = this.props.people[index]
+    render() {
         return (
-            <div style={style} key={key}>
-                <PersonCard person = {person}/>
-            </div>
+            <TransitionMotion
+                willEnter={this.willEnter}
+                styles={this.getStyles}
+            >
+                {interpolated =>
+                    <List
+                        rowRenderer={this.rowRenderer(interpolated)}
+                        rowCount={interpolated.length}
+                        rowHeight={100}
+                        height={400}
+                        width={400}
+                        ref={this.setListRef}
+                    />
+                }
+            </TransitionMotion>
         )
     }
+
+    rowRenderer = interpolated => ({index, key, style}) =>
+        <PersonCard person={this.props.people[index]} key={key}
+                    style={{...style, ...interpolated[index].style}}/>
+
+    willEnter = () => ({
+        opacity: 0
+    })
+
+    getStyles = () => this.props.people.map(person => ({
+        key: person.uid,
+        style: {
+            opacity: spring(1)
+        },
+        data: person
+    }))
+
+    setListRef = ref => this.list = ref
 }
 
-export default connect((state) => ({
+export default connect(state => ({
     people: peopleListSelector(state)
 }), { fetchAllPeople })(PeopleList)
