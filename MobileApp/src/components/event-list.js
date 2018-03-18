140a7428b3ce487cc355b52c7f6c442a08b634c1
alex_kart <alex_kart@mail.ru>
2018-03-18 22:45:17 +0200

    HW6

--- MobileApp/src/components/event-list.js
+++ MobileApp/src/components/event-list.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react'
-import {View, Text, ScrollView, StyleSheet} from 'react-native'
+import {View, Text, ScrollView, StyleSheet, SectionList} from 'react-native'
 import Card from './common/card'
 
 class EventList extends Component {
     static propTypes = {
-
     };
 
+
+
+
     render() {
+        let {events} = this.props
+        events = events.sort((a,b) => a.title < b.title );
+        let sections = {}
+        events.forEach(item => {
+            if (sections[item.title[0]])
+                sections[item.title[0]].push(item)
+            else
+                sections[item.title[0]] = [item]
+        } )
+
+        sections = Object.entries(sections).map( ([key, value]) => ({data: value, key: key, title: `${key} (${value.length})` }));
+
         return (
-            <ScrollView>
-                {this.props.events.map(event =>
-                    <Card key = {event.uid}>
-                        <Text>{event.title}</Text>
-                    </Card>
-                )}
-            </ScrollView>
+            <SectionList
+                renderItem={({item}) => <Card key = {item.uid}>
+                                            <Text>{item.title}</Text>
+                                        </Card>}
+                renderSectionHeader={({section}) => <Text>{section.title}</Text>}
+                keyExtractor={item => item.uid}
+                sections={sections}
+            />
         )
     }
 }
